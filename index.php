6dc4a49fda0c3a47c58139ebaec54e94d4d810b6
Doug <douglas.springer@gmail.com>
2017-12-19 01:54:25 -0500

    Classes 101

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>PHPPractitioner</title>
-    </head>
-    <body>
- 		<h1>Hello World</h1>       
-    </body>
-</html>
+<?php
+
+class Task {
+	public $description;
+
+	public $completed = false;
+
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+
+	public function isComplete()
+	{
+		return $this->completed;		
+	}
+
+	public function complete()
+	{
+		$this->completed = true;
+	}
+}
+
+$tasks = [
+	new Task('Go to the store'),
+	new Task("Finish my screencast"),
+	new Task('Clean my room')
+];
+
+$tasks[0]->complete();
+
+require 'index.view.php';
