f98ebbfe27ede3d9c364026cd0c6d3fa853a4903
Doug <douglas.springer@gmail.com>
2017-12-31 15:56:27 -0500

    Dynamic Inserts with PDO

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -12,7 +12,27 @@ class QueryBuilder
 	public function selectAll($table)
 	{
 		$statement = $this->pdo->prepare("select * from {$table}");
+
 		$statement->execute();
+
 		return $statement->fetchAll(PDO::FETCH_CLASS);
+	}
+
+	public function insert($table, $parameters)
+	{
+		// die(var_dump(array_keys($parameters)));
+		$sql = sprintf(
+			'insert into %s (%s) values (%s)',
+			$table,
+			implode(', ', array_keys($parameters)),
+			':' . implode(', :', array_keys($parameters))
+		);
+		try{
+			$statement = $this->pdo->prepare($sql);
+
+			$statement->execute($parameters);
+		} catch (Exception $e) {
+			die('Whoops, something went wrong.');
+		}
 	}
 }
--- routes.php
+++ routes.php
@@ -5,6 +5,3 @@ $router->get('about', 'controllers/about.php');
 $router->get('about/culture', 'controllers/about-culture.php');
 $router->get('contact', 'controllers/contact.php');
 $router->post('names', 'controllers/add-name.php');
-
-// var_dump($router->routes);
-// die();
